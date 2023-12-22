0647b4c97a7e5d5333fafc9971f4bd54da3c6748
ChadhaH <chadha.hamed1@gmail.com>
2023-12-22 12:20:59 -0800

    all

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #!/usr/bin/pup
-# installing flask from pip3
-
-package { 'flask':
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip'
+  provider => 'pip3'
 }
